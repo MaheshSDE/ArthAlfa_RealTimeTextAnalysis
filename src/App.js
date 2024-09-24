8488d91b25ed00c31c20344c77969281ea9e13bc
mahesh <pmahesh493605@gmail.com>
2024-09-24 11:36:59 +0530

    text-analysis-and-string-replacement

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TextAnalysisAndStringReplacement from "./components/TextAnalysisAndStringReplacement"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextAnalysisAndStringReplacement/>
+    </div>
+  );
+}
+
+export default App;
